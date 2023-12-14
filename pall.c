7422b54b41ecc17bad80c10d0175f9e2330fd75c
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-12-14 09:29:30 -0800

    feat: add pall.c file

---
+++ pall.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * f_pall - Prints the elements of the stack.
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number (not used in this function)
+ * Return: No return value.
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+stack_t *h;
+/* Unised paarameter to prevent compiler warning */
+(void)counter;
+
+h = *head;
+
+/* If stack is empty, no need to print anything */
+if (h == NULL)
+return;
+
+/* Traverse the stack and print each element */
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+}
