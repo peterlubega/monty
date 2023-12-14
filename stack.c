cc765d43c0dcc761e19d4bb9bd9444c820f327ea
peterlubega <*peterlubega100@gmail.com>
2023-12-14 09:43:43 -0800

    feat: adding the stack.c file

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - Sets the stack behavior to stack (LIFO).
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number where the operation is called.
+ * Return: No return value.
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+(void)head;
+(void)counter;
+/* Set the stack behavior to stack (LIFO) by updating the bus. */
+bus.lifi = 0;
+}
