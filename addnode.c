b41395d49dc364ed90c94b8d6280c0fae473a15a
peterlubega <*peterlubega100@gmail.com>
2023-12-14 09:17:48 -0800

    feat: adding the addnode c file

---
+++ addnode.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * addnode - adds a new node to the head of the stack
+ * @head: pointer to the head of the stack
+ * @n: value of the new node
+ * Return: no return
+ */
+void addnode(stack_t **head, int n)
+{
+
+stack_t *new_node, *aux;
+
+/* Save the current head of the stack */
+aux = *head;
+
+/* Allocate memory for the new node */
+new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+printf("Error\n");
+exit(0); }
+
+/* If the stack is not empty, update the previous pointer of the current head */
+if (aux)
+aux->prev = new_node;
+
+/* Initialze thee new node with the given value and connections */
+new_node->n = n;
+new_node->next = *head;
+new_node->prev = NULL;
+
+/* Update the head of the stack to point to the new node */
+*head = new_node;
+}
