7170944d1e7ce49d5f437c042bb822c98477247e
peterlubega <*peterlubega100@gmail.com>
2023-12-14 10:26:40 -0800

    fix: fixing the file name

--- execute.c
+++ execute.c
@@ -1,4 +1,4 @@
-#include "monty.h"
+g#include "monty.h"
 
 /**
  * execute - executes the opcode.
@@ -8,25 +8,15 @@
  * @file: poiner to Monty file
  * Return: 0 on success, 1 on failure
  */
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+int execute(char *content, stack_t **stack, unsigned int counter, FILE * file)
 {
 instruction_t opst[] = {
 {"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-{"pop", f_pop},
-{"swap", f_swap},
-{"add", f_add},
-{"nop", f_nop},
-{"sub", f_sub},
-{"div", f_div},
-{"mul", f_mul},
-{"mod", f_mod},
-{"pchar", f_pchar},
-{"pstr", f_pstr},
-{"rotl", f_rotl},
-{"rotr", f_rotr},
-{"queue", f_queue},
-{"stack", f_stack},
-{NULL, NULL}
+{"pop", f_pop}, {"swap", f_swap}, {"add", f_add},
+{"nop", f_nop}, {"sub", f_sub}, {"div", f_div},
+{"mul", f_mul}, {"mod", f_mod}, {"pchar", f_pchar},
+{"pstr", f_pstr}, {"rotl", f_rotl}, {"rotr", f_rotr},
+{"queue", f_queue}, {"stack", f_stack}, {NULL, NULL}
 };
 unsigned int i = 0;
 char *op;
